41f7f96c1173a9d095ec15589239c4843a0f8fd8
luckyfoxdesign <luckyfoxdesign@yandex.ru>
2024-05-25 01:01:07 +0200

    added request sample to remote mysql

---
+++ src/routes/api/getm/+server.js
@@ -0,0 +1,37 @@
+import { error, json } from '@sveltejs/kit';
+import mysql from 'mysql2/promise';
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const connection = await mysql.createConnection({
+    host: dbHost,
+    user: dbUser,
+    password: dbPassword,
+    database: dbName,
+});
+
+async function getPostFromDatabase() {
+    try {
+        const [results, fields] = await connection.query(
+            'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45'
+        );
+
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        return results;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+    const res = await getPostFromDatabase()
+
+    console.log(res)
+
+    return json(res);
+}
